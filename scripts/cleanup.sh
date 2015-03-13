423c2e293fe2e9cb0cee04f485b1184c4178b0e0
Chris <ctarwater@madsecurity.com>
2015-03-13 12:42:08 -0700

    proper cleanup script

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,5 +1,6 @@
 # Remove SSH key pairs according to AWS requirements for shared AMIs: http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/building-shared-amis.html
 sudo shred -u /etc/ssh/*_key /etc/ssh/*_key.pub
 
-# Remove all history files
-shred -u ~/.*history
+# Clear our Bash history
+history -c
+history -w
